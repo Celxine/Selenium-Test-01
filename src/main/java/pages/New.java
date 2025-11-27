24ea8272d3df30b7b230fba7bba641500d929021
celxine <ingabireceline54@gmail.com>
2025-11-27 18:20:13 +0200

    b

---
+++ src/main/java/pages/New.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class New {
+    private WebDriver driver;
+    public New(WebDriver driver){
+        this.driver= driver;
+    }
+
+}
